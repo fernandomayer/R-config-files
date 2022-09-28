41a8d9f26eb140b6f7e41962a039d2ff62fefcb0
Fernando Mayer <fernandomayer@gmail.com>
2022-09-28 16:28:40 +0100

    Update how we update

--- update.R
+++ update.R
@@ -1,35 +1,37 @@
 #!/usr/bin/env Rscript
 
 args <- commandArgs(trailingOnly = TRUE)
-if(length(args) == 0) stop("We need args here")
+## if(length(args) == 0) stop("We need args here")
 
 div <- paste0("##", paste(rep("=", 70), collapse=""))
 
-cat(div, "\n", "==> Package Status\n\n")
+cat(div, "\n", "==> Package status\n\n")
 (up <- packageStatus())
 ups <- summary(up)
 
-cat("\n\n ==> Número de pacotes a serem atualizados\n\n")
+cat("\n\n ==> Number of packages to be updated\n\n")
 nup <- lapply(ups$Libs, function(x) x$upgrade)
 nup$Total <- length(unlist(nup))
 nup
 
 Sys.sleep(5)
 
-cat(div, "\n", "==> Update Packages\n\n")
-
-if(length(unlist(nup)) == 0) {
-    cat(" => Nenhum pacote a ser atualizado\n")
+if(nup$Total == 0) {
+    cat(" => No packages to be updated\n")
 } else {
+    cat(div, "\n", "==> Update packages\n\n")
     update.packages(ask = FALSE)
 }
 
-cat("\n", div, "\n", "==> Update Remotes\n")
-
-for(i in seq_along(args)) {
-    cat("\n", sprintf(" => Atualizando %s", args[i]),
-        "--------------------", "\n\n")
-    remotes::install_github(args[i])
+if(length(args) != 0) {
+    cat("\n", div, "\n", "==> Update remotes\n")
+    for(i in seq_along(args)) {
+        cat("\n", sprintf(" => Updating %s", args[i]),
+            "--------------------", "\n\n")
+        remotes::install_github(args[i])
+    }
+} else {
+    cat(" => No Remotes specified\n")
 }
 
 cat("\n ==> END UPDATE\n", div, "\n\n")
